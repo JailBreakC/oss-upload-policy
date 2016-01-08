ff0b7d5ab8480c9304adbd9943d8ca6993cc35a7
JailBreak <admin@vgee.cn>
2016-01-08 17:49:30 +0800

    add cros & ingore

--- index.js
+++ index.js
@@ -5,11 +5,19 @@ var router = express.Router();
 var policy = require('./lib');
 app.set('port', 80);
 
+var cors = function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    //console.log(req.user);
+    next();
+};
+
 router.route('/policy')
 	.post(policy.policy)
 	.get(policy.policy);
 
 app.use('/test', express.static(path.join(__dirname, './test')));
+app.use(cors);
 app.use(router);
 
 var server = app.listen(app.get('port'), function() {
